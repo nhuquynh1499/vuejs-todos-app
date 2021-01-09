c89af727159f90e9ddc0635377511fd805347e11
nhuquynh1499 <ngngocnhuquynh0104@gmail.com>
2021-01-09 12:56:22 +0700

    Add item for todos list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <input type="text" v-model="newTodo"/> 
+    <button @click="addItem">OK</button>
+
+  </div>
+
+  <ul>
+    <ToDoItem v-for="todo in todos" 
+      :key="todo._id" 
+      :content="todo.content"
+      :isCompleted="todo.isCompleted"
+    >
+    </ToDoItem>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue';
+const shortid = require('shortid');
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ToDoItem
+  },
+  data() {
+    return {
+      name: "Quynh",
+      newTodo: "",
+      todos: [{
+        _id: "1789db9a-2a3b-453e-87e4-a6749c5f2f7e",
+        content: "Learn English",
+        isCompleted: true,
+      }, {
+        _id: "eb5332b1-d9f1-4af2-8118-e71eb909b350",
+        content: "Go to market",
+        isCompleted: true,
+      }, {
+        _id: "97fe55b0-15b5-4262-99b1-588c77c10aa1",
+        content: "Buy something",
+        isCompleted: false,
+      }, {
+        _id: "9897ecfd-9244-4c53-af2f-9fd1c4db2d48",
+        content: "Cook a dinner",
+        isCompleted: false,
+      }]
+    }
+  },
+  methods: {
+    addItem() {
+      console.log("add");
+
+      const id = shortid.generate();
+      const newItem = {
+        _id: id,
+        content: this.newTodo,
+        isCompleted: false
+      }
+      this.todos.push(newItem);
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ul {
+  list-style-type: none;
 }
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <li :class="{ completed: isCompleted }">
+        <input type="checkbox" :checked="isCompleted"/> 
+        <span >{{ content }}</span>
+    </li>    
+</template>
+
+<script>
+export default {
+    props: ['content', 'isCompleted']
+}
+</script>
+
+<style lang="scss">
+    .completed {
+        span {
+            text-decoration: line-through;
+            color: #b2b2b2;
+        }
+    }
+</style>
