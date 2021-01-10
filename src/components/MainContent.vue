e3546260b528eb9447ebb8989d7a93d154b4f209
nhuquynh1499 <ngngocnhuquynh0104@gmail.com>
2021-01-10 10:08:52 +0700

    Fix UI

---
+++ src/components/MainContent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <span >{{ content }}</span>
+        <input type="text" :class="{ actived: _id === activeId }" :value="content"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['_id', 'content', 'activeId'],
+}
+</script>
+
+<style lang="scss" scoped>
+    input[type="text"] {
+        display: none;
+    }
+
+    input[type="text"].actived {
+        display: block;
+    }
+</style>
